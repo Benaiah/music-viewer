eda90b29564f8beb50456d438c478ca2630bbd8d
Benaiah Mischenko <benaiah@mischenko.com>
2016-02-04 15:50:13 -0800

    Sort list of songs alphabetically

--- app/src/components/SongList.jsx
+++ app/src/components/SongList.jsx
@@ -3,7 +3,13 @@ import SongListItem from './SongListItem'
 
 const SongList = ({songs, songClickAction}) => {
   return <ul className="song-list">
-      {songs.items.map(song => <SongListItem key={song.slug} song={song} />)}
+      {songs.items.slice()
+            .sort((a, b) => {
+              const tA = a.name.toUpperCase()
+              const tB = b.name.toUpperCase()
+              return (tA < tB) ? -1 : (tA > tB) ? 1 : 0
+            })
+            .map(song => <SongListItem key={song.slug} song={song} />)}
   </ul>
 }
 
